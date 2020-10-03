13649a8a1cb52a2146dda4deffe3a4f40ead072a
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2020-10-03 16:52:05 -0300

    teste criar array com valores do patrimonio

--- teste-perfil.js
+++ teste-perfil.js
@@ -1,11 +1,20 @@
+// URL com dados da candidata
 const url = "https://api-perfilpolitico.serenata.ai/api/candidate/2309762/"
+let dataset = [];
+
+//Coletando o dado.
 d3.json(url)
     .then(res => {
-        //alerta a quantidade de vezes que o candidato se candidatou
-       alert(res.election_history.length);
+        //alerta o valor do patrimonio da candidata.
+       alert(res.asset_history[1].value);
+
+       for (let index = 0; index < asset_history.length; index++) {
+           const element = asset_history[index];
+            dataset.push(element.value)
+            alert(element.value)
+       }
         
     })
     .catch(() => {
         console.log('deu errado')
     })
-

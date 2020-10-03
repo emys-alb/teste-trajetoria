5ed0011b03252e8d59d8ee10eb270ef7544d3487
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2020-10-03 16:21:50 -0300

    Descobrindo como visualizar o dado

--- teste-perfil.js
+++ teste-perfil.js
@@ -1,8 +1,11 @@
 const url = "https://api-perfilpolitico.serenata.ai/api/candidate/2309762/"
 d3.json(url)
     .then(res => {
-        console.log("deu bom")
+        //alerta a quantidade de vezes que o candidato se candidatou
+       alert(res.election_history.length);
+        
     })
     .catch(() => {
         console.log('deu errado')
     })
+

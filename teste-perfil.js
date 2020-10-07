141e2fd7e787363b26862ae6350493ca7b604d01
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2020-10-07 18:33:30 -0300

    Teste circulos no svg

--- teste-perfil.js
+++ teste-perfil.js
@@ -2,15 +2,15 @@
 const url = "https://api-perfilpolitico.serenata.ai/api/candidate/2309762/"
 
 //Coletando o dado.
-d3.json(url)
-    .then(res => {
-        //alerta o valor do patrimonio da candidata.
-       //alert(res.asset_history[1].value);
-        
-    })
-    .catch(() => {
-        console.log('deu errado')
-    })
+var dados = d3.json(url)
+        .then(res => {
+            //alerta o valor do patrimonio da candidata.
+        //alert(res.asset_history[1].value);
+            
+        })
+        .catch(() => {
+            console.log('deu errado')
+        })
 
 var 
     width = 880,
@@ -21,6 +21,21 @@ var viewBox = {
     height: height
 }
 
+var data = [8, 56, 40 ,3 ,34, 20]
+
 //Adiciona elemento no html.
-var svg = d3.select("#grafico").append("svg")
-    .attr("viewBox", "0 0 " + viewBox.width + " " + viewBox.height);
+d3.select("#grafico").append("svg")
+    .attr("viewBox", "0 0 " + viewBox.width + " " + viewBox.height)
+    .attr("id", "canvas");
+
+
+//Criando circulos no svg
+d3.select("#canvas")
+    .selectAll("circle")
+    .data(data)
+    .join("circle")
+        .attr("cx", data => data * 10)
+        .attr("cy", 200)
+        .attr("r", 8)
+        .attr("fill", "purple");
+

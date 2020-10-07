9193b7e32d345cefd3c04c310d9eaf32139eb00c
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2020-10-07 16:15:03 -0300

    Adicionando elemento de seleção

--- teste-perfil.js
+++ teste-perfil.js
@@ -1,20 +1,26 @@
 // URL com dados da candidata
 const url = "https://api-perfilpolitico.serenata.ai/api/candidate/2309762/"
-let dataset = [];
 
 //Coletando o dado.
 d3.json(url)
     .then(res => {
         //alerta o valor do patrimonio da candidata.
-       alert(res.asset_history[1].value);
-
-       for (let index = 0; index < asset_history.length; index++) {
-           const element = asset_history[index];
-            dataset.push(element.value)
-            alert(element.value)
-       }
+       //alert(res.asset_history[1].value);
         
     })
     .catch(() => {
         console.log('deu errado')
     })
+
+var 
+    width = 880,
+    height = 880;
+    
+var viewBox = {
+    width: width,
+    height: height
+}
+
+//Adiciona elemento no html.
+var svg = d3.select("#grafico").append("svg")
+    .attr("viewBox", "0 0 " + viewBox.width + " " + viewBox.height);

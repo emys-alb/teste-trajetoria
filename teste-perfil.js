16cc78ce61c327d9e0b7668fbd4e01837e891f5c
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2020-10-11 16:43:47 -0300

    Finaliza os círculos do gráfico

--- teste-perfil.js
+++ teste-perfil.js
@@ -16,20 +16,20 @@ d3.json(url)
         .append("svg")
         .attr("width", width)
         .attr("height", height)
-        .style("padding", "30px 60px 30px 30px")
+        .style("padding", "30px 65px 30px 30px")
         .attr("id", "svg")
 
         const g = svg.append("g")
-            .attr("transform", 'translate(' + 60 + ', ' + 30 + ')')
+            .attr("transform", 'translate(' + 65 + ', ' + 30 + ')')
         
         //Criando escalas
         const escalaX = d3.scaleTime()
-            .domain([affillitions[0].started_in, Date.now])
-            .rangeRound([0, width])
+            .domain([Date.parse(affillitions[0].started_in), Date.now()])
+            .range([0, width])
 
         const escalaY = d3.scaleLinear()
             .domain([d3.min(assets, (d) => d.value), d3.max(assets, (d) => d.value)])
-            .range([height, 0])
+            .range([height - 60, 0])
 
         //Criar circulos com os valores do patrimonio    
         g.selectAll("circle")
@@ -37,12 +37,13 @@ d3.json(url)
             .enter()
             .append("circle")
             .attr("class", "assetValues")
-            .attr("cx", (d) => escalaX(d.year))
+            .attr("cx", (d) => escalaX(new Date(d.year, 0, 1)))
             .attr("cy", (d) => escalaY(d.value))
-            .attr("r", 8)
+            .attr("r", 7)
             .attr("fill", "#6f42c1")
 
         //Criar retas entre os valores TODO
+
         
         //Preencher area TODO
         
